3091482f3f55bc28c5af4e54f12d98977ae53cd0
rong5026 <rong5026@naver.com>
2024-03-10 19:09:42 +0900

    [15]

--- servlet/src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
+++ servlet/src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
@@ -5,7 +5,9 @@ import java.nio.charset.StandardCharsets;
 
 import org.springframework.util.StreamUtils;
 
-import hello.servlet.basic.HelloServlet;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import hello.servlet.basic.HelloData;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.annotation.WebServlet;
@@ -13,13 +15,18 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
-public class requestBodyStringServlet extends HttpServlet {
+@WebServlet(name = "requestBodyJsonServlet", urlPatterns = "/request-body-json")
+public class RequestBodyJsonServlet extends HttpServlet {
+	private ObjectMapper objectMapper = new ObjectMapper();
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		ServletInputStream inputStream = request.getInputStream();
 		String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
 		System.out.println("messageBody = " + messageBody);
+
+		HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+		System.out.println("helloData.username = " + helloData.getUsername());
+		System.out.println("helloData.age = " + helloData.getAge());
 		response.getWriter().write("ok");
 	}
 }
